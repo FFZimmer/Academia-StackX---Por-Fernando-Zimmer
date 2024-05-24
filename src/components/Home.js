3a67a9340e6faf1a73ac369aa22f2c1dabd74782
Fernando Falkemback Zimmer <108485819+FFZimmer@users.noreply.github.com>
2024-05-24 11:17:28 -0300

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <main className="App-home">
+      <h2>Bem-vindo à Academia StackX</h2>
+      <p>Explore nossos cursos e comece a aprender hoje mesmo!</p>
+    </main>
+  );
+}
+
+export default Home;
